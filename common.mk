0fae48c063d4e0b7968c17a7828b349b22d39d27
Łukasz Patron <priv.luk@gmail.com>
2024-09-27 05:15:12 +0800

    mt6789-common: Build AIDL NXP NFC HAL
    [Shirayuki39] and drop prebuilt NFC Blobs
    
    Signed-off-by: Shirayuki39 <lorddemecrius83@proton.me>

--- common.mk
+++ common.mk
@@ -262,7 +262,7 @@ PRODUCT_COPY_FILES += \
 
 # NFC
 PRODUCT_PACKAGES += \
-    android.hardware.nfc@1.2.vendor \
+    android.hardware.nfc-service.nxp \
     com.android.nfc_extras \
     NfcNci \
     SecureElement \
